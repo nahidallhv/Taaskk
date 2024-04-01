629887e008ed203f2dfe85d5774dac4f1c882600
nahidallhv <161585836+nahidallhv@users.noreply.github.com>
2024-04-01 11:38:38 +0400

    tt

---
+++ app.js
@@ -0,0 +1,116 @@
+// 1. Obyekt yaradin
+// let students = {
+//     name: "Nahid",
+//     surname:  "Allahverdiyev",
+//     age:  19,
+//     adress: "Naxcivan MR",
+// }
+// a.
+// console.log(students);
+
+// b.
+// console.log(students.name);
+// console.log(students.age);
+
+// 2. Array icinde obyektler yaradin
+
+// let students = [
+//     {
+//       name: "Nahid" ,
+//       surname: "Allahverdiyev",
+//       adress: "Naxcivan MR",
+//     },
+//     {
+//       name: "Nadir" ,
+//       surname: "Haciyev",
+//       adress: "Naxcivan MR",
+//     },
+//     {
+//       name: "Firuze" ,
+//       surname: "Mehdiyeva",
+//       adress: "Ordubad",
+//     }
+// ]
+// a .
+// console.log(students);
+// b.
+// console.log(students[0].name);
+// console.log(students[1].adress);
+
+// 3. Telebeler obyektinde bir nece telebe elave edin. Hobbiler, xususiyyetleri.
+
+// let students = [
+//     {
+//       name: "Nahid" ,
+//       surname: "Allahverdiyev",
+//       adress: "Naxcivan MR",
+//       hobbi: [ "Gitar", "Futbol", "voleybol"],
+//       characterics: [ "yarasiqli", "agilli"]
+//     },
+//     {
+//       name: "Nadir" ,
+//       surname: "Haciyev",
+//       adress: "Naxcivan MR",
+//       hobbi: [ "kitab oxumaq"],
+//       characterics: [ "agilli", "disiplinli"]
+//     },
+//     {
+//       name: "Firuze" ,
+//       surname: "Mehdiyeva",
+//       adress: "Ordubad",
+//       hobbi: [ "oyun oynamaq","filme baxmaq"],
+//       characterics: [ "Gozel",]
+//     }
+// ]
+// a .
+// console.log(students);
+// b.
+// console.log(students[0].name);
+// console.log(students[0].hobbi);
+// console.log(students[1].characterics);
+
+
+let students = [
+  {
+    name: "Nahid",
+    surname: "Allahverdiyev",
+    point: 70,
+  },
+  {
+    name: "Nadir",
+    surname: "Haciyev",
+    point: 80,
+  },
+  {
+    name: "Firuze",
+    surname: "Mehdiyeva",
+    point: 90,
+  },
+  {
+    name: "Meleyke",
+    surname: "Huseynova",
+    point: 96,
+  },
+];
+
+// 4.Students arrayinin qiymet ortalamasini cixardin.
+// let c = 0;
+// let s = 4;
+// for (let i = 0; i < students.length; i++) {
+//   c += students[i].point;
+// }
+// let eorta = c / s;
+// console.log("Ortalama : ", eorta);
+
+
+
+// 5. Qiymeti 90-dan asagi olanlari ekrana cixart ve xeberdarlq et
+// for (let i = 0; i < students.length; i++) {
+//       if(students[i].point < 90){
+//         console.log(`${students[i].name} ${students[i].surname}` + " Qiymetiniz 90-dan asagidir ");
+
+//       }
+      
+//     }
+
+
